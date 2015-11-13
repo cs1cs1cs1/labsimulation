f828fbb1f8f5524080a8166acffdd45bce328299
Neha Manu <nm8ff@virginia.edu>
2015-11-12 19:41:53 -0500

    c and d

--- simulation/catFun.py
+++ simulation/catFun.py
@@ -89,11 +89,10 @@ def endState(state):
 def handleEvent(state, event):  
 #    print("Handling event: " + str(event))
     if (event.type == pg.MOUSEBUTTONDOWN):
-        if (state[1]) == 1:
-            newState = -1
-        else:
-            newState = 1   
-        return((state[0],newState))
+        vx = randint(-5,5)
+        vy = randint(-5,5)
+          
+        return(state[0], state[1], vx, vy)
     else:
         return(state)
 
@@ -102,10 +101,10 @@ def handleEvent(state, event):
 # World state will be single x coordinate at left edge of world
 
 # The cat starts at the left, moving right 
-initState = (0,1,1,1)
+initState = (randint(1,5),randint(1,5),randint(-5,5),randint(-5,5))
 
 # Run the simulation no faster than 60 frames per second
-frameRate = 30
+frameRate = 60
 
 # Run the simulation!
 rw.runWorld(initState, updateDisplay, updateState, handleEvent,
